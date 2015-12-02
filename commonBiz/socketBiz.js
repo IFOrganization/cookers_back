65f540066d9802ad8efe1a6e06c4234e03678c2c
yoojasung <airnold@naver.com>
2015-12-02 17:43:03 +0900

    socket io 서버 완료

--- commonBiz/socketBiz.js
+++ commonBiz/socketBiz.js
@@ -4,14 +4,32 @@
 
 var socket_biz = {};
 
-socket_biz.checkSocketId = function(list, id){
 
+socket_biz.findSocketId = function(list, touserid){
+    /**
+     * 해당하는 모든 아이디가 있는 인덱스 찾아 가져오기
+     */
+
+    var finduser = [];
+
+    for(var i in list){
+        if(list[i].userid == touserid){
+            finduser.push(list[i]);
+        }
+    }
+    return finduser;
 };
 
-socket_biz.findSocketId = function(list, id){
-
+socket_biz.removeSocketId = function(list, socketid){
+    /**
+     * 한개만 삭제하면 됨
+     */
+    for(var i in list){
+        if(list[i].socketid == socketid){
+            list.splice(i, 1);
+            return list;
+        }
+    }
 };
 
-socket_biz.removeSocketId = function(list, id){
-
-};
+module.exports = socket_biz;
--- server_setting/serverrun.js
+++ server_setting/serverrun.js
@@ -5,29 +5,64 @@
 
 var http = require('http');
 var userList = [];
+var socket_biz = require('../commonBiz/socketBiz.js');
 
 exports.serverStart = function(app){
-
 
     var server = http.createServer(app);
     server.listen(app.get('port'), function(){
         console.log('Cookers server running at ' + app.get('port'));
     });
 
+
+    /**
+     * socket 코딩 부분
+     */
+
     var io = require('socket.io').listen(server);
 
     io.on('connection', function(socket){
-        socket.emit('success', '연결 성공 ! ');
+        socket.emit('success', '연결 성공 !');
 
         socket.on('add user', function(data){
+
+            var temp =  {};
+            temp.socketid = socket.id;
+            temp.userid = data;
+            userList.push(temp);
+
+        });
+
+        socket.on('notipush', function(touserid){
             /**
-             * 현재 userList에 있는지 체크후 저장해주기.
-             * @type {{socket: *}}
+             * user id 현재 접속되어있는지 찾기
              */
 
-            userList[data] = {
-                "socket" : socket.id
-            };
+            var sendlist = socket_biz.findSocketId(userList, touserid);
+
+            if(sendlist.length == 0){
+                /**
+                 * 접속되있지 않음 apn, gcm 사용
+                 */
+            }else{
+                /**
+                 * 접속되어있음 보내야 함
+                 */
+
+                for(var i in sendlist){
+                    io.to(sendlist[i].socketid).emit('badgenoti', '!');
+                }
+            }
+        });
+
+        socket.on('disconnect', function(){
+
+            userList = socket_biz.removeSocketId(userList, socket.id);
+            console.log(socket.id);
+            /**
+             * socket.id 에 해당하는 객체 삭제 해주기
+             */
+
         });
     })
 
